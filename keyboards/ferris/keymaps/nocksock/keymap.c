a4f26d4acc7e46ae0dd24e7014a121e1d1c87f95
Nils Riedemann <nils@bleepbloop.studio>
2022-10-17 17:46:10 +0200

    swap layers left right

--- keyboards/ferris/keymaps/nocksock/keymap.c
+++ keyboards/ferris/keymaps/nocksock/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q         , KC_W         , KC_E         , KC_R         , KC_T , KC_Y , KC_U         , KC_I         , KC_O         , KC_P            ,
     LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D) , LSFT_T(KC_F) , KC_G , KC_H , RSFT_T(KC_J) , RCTL_T(KC_K) , RALT_T(KC_L) , RGUI_T(KC_SCLN) ,
     LT(3         , KC_Z)        , KC_X         , KC_C         , KC_V , KC_B , KC_N         , KC_M         , KC_COMM      , KC_DOT          , LT(3 , KC_SLSH) ,
-          LT(1, KC_SPC), HYPR_T(KC_BSPC), HYPR_T(KC_ESC), LT(2, KC_ENT)
+          LT(2, KC_SPC), HYPR_T(KC_BSPC), HYPR_T(KC_ESC), LT(1, KC_ENT)
   ),
 
   // symbol layer
