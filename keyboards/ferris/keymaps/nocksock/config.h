163cc61f8458a7d3d2e36d57f7c29e439a944ace
Nils Riedemann <nils@bleepbloop.studio>
2022-12-16 21:52:35 +0100

    feat: we, xc, io and ,. combos, bilateral combos
    curios to see how combos feel.

--- keyboards/ferris/keymaps/nocksock/config.h
+++ keyboards/ferris/keymaps/nocksock/config.h
@@ -22,4 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 // #define TAPPING_FORCE_HOLD
-
+#define BILATERAL_COMBINATIONS 500
+#define COMBO_COUNT 4
--- keyboards/ferris/keymaps/nocksock/keymap.c
+++ keyboards/ferris/keymaps/nocksock/keymap.c
@@ -5,6 +5,18 @@
 #define KC_LTZ LT(3, KC_Z) 
 #define KC_LTSLSH LT(3, KC_SLSH) 
 
+const uint16_t PROGMEM comb_we[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM comb_xc[] = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM comb_io[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM comb_comdot[] = {KC_COMM, KC_DOT, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(comb_we, KC_ESC),
+    COMBO(comb_xc, KC_TAB),
+    COMBO(comb_io, KC_BSPC),
+    COMBO(comb_comdot, KC_ENT)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // base layer
   [0] = LAYOUT( 
@@ -16,32 +28,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
   // symbol layer
   [1] = LAYOUT( 
-      KC_EXLM  ,   KC_AT , KC_HASH ,  KC_DLR , KC_PERC ,  KC_CIRC , KC_AMPR  ,  KC_ASTR , KC_DQUO  , KC_MINUS ,
-      KC_EQUAL , KC_LBRC , KC_LPRN , KC_RPRN , KC_RBRC , KC_QUOTE , MOD_RSFT , MOD_RCTL , MOD_RALT , MOD_RGUI ,
-      KC_GRV   , XXXXXXX , XXXXXXX , XXXXXXX , KC_BSLS ,  XXXXXXX , KC_UNDS  ,  KC_LABK , KC_RABK  ,  KC_SLSH ,
+      KC_EXLM  , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC  , KC_AMPR       , KC_ASTR       , KC_DQUO       , KC_MINUS      ,
+      KC_EQUAL , KC_LCBR , KC_LPRN , KC_RPRN , KC_RCBR , KC_QUOTE , MOD_RSFT , MOD_RCTL , MOD_RALT , MOD_RGUI ,
+      KC_GRV   , XXXXXXX , KC_LBRC , KC_RBRC , KC_BSLS , XXXXXXX  , KC_UNDS       , KC_LABK       , KC_RABK       , KC_SLSH       ,
               LT(4, KC_SPC), LALT(KC_BACKSPACE), _______, _______
   ),
 
   // extension layer (navigation)
   [2] = LAYOUT( 
-      KC_TAB       , XXXXXXX       , KC_MPRV       , KC_MNXT       , XXXXXXX , KC_HOME , KC_PGDN , KC_PGUP , KC_END   , CAPSWRD ,
-      MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , KC_MPLY , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , KC_DEL,
-      XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX       , XXXXXXX , KC_TAB  , KC_ENT  , XXXXXXX , XXXXXXX  , XXXXXXX,
+      KC_TAB   ,  XXXXXXX , KC_MPRV  ,  KC_MNXT , XXXXXXX , KC_HOME , KC_PGDN , KC_PGUP , KC_END   , CAPSWRD ,
+      MOD_LGUI , MOD_LALT , MOD_LCTL , MOD_LSFT , XXXXXXX , KC_LEFT , KC_DOWN ,   KC_UP , KC_RIGHT ,  KC_DEL ,
+      XXXXXXX  ,  XXXXXXX , XXXXXXX  ,  KC_MPLY , XXXXXXX ,  KC_TAB , KC_ENT  , XXXXXXX , XXXXXXX  , XXXXXXX ,
                         _______ , _______ , _______ , MO(4)
   ),
 
   // num layer
   [3] = LAYOUT( 
       KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7 , KC_8 , KC_9 , KC_0    ,
-      _______ , _______ , _______ , _______ , XXXXXXX , XXXXXXX , RSFT_T(KC_4) , RCTL_T(KC_5) , RALT_T(KC_6) , _______ ,
+      _______ , _______ , _______ , _______ , XXXXXXX , XXXXXXX , KC_4 , KC_5 , KC_6 , _______ ,
       XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_0    , KC_1 , KC_2 , KC_3 , XXXXXXX ,
                                     _______ , _______ , _______ , _______
   ),
 
   // fkey-layer
   [4] = LAYOUT( 
-      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , QK_BOOT , KC_F12 , KC_F7 , KC_F8 , KC_F9 , XXXXXXX ,
-      _______ , _______ , _______ , _______ , QK_REBOOT , KC_F11 , KC_F4 , KC_F5 , KC_F6 , XXXXXXX ,
+      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F12 , KC_F7 , KC_F8 , KC_F9 , QK_BOOT ,
+      _______ , _______ , _______ , _______ , XXXXXXX , KC_F11 , KC_F4 , KC_F5 , KC_F6 , XXXXXXX ,
       XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F10 , KC_F1 , KC_F2 , KC_F3 , XXXXXXX ,
                        _______, _______, _______, _______
   )
