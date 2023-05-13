8977a1daadd96c478ddf73cd8f8c5191294ddf7e
Nils Riedemann <nils@bleepbloop.studio>
2023-05-13 22:13:32 +0200

    feat(ferris/sweep:nocksock): swapping thumbs... again

--- keyboards/ferris/keymaps/nocksock/keymap.c
+++ keyboards/ferris/keymaps/nocksock/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_Q,   KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I,    KC_O,   KC_P,
           KC_A,   KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
           KC_LTZ, KC_X, KC_C, KC_V, KC_B,        KC_N, KC_M, KC_COMM, KC_DOT, KC_QUOT,
-          KC_SPC , LA_NAV , LA_SYM, RSFT_T(KC_ESC)
+          LA_NAV , KC_SPC , RSFT_T(KC_ESC), LA_SYM
     ),
 
     [SYM] = LAYOUT(
