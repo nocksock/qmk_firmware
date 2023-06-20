d893bf9e1c3aedb23b870a4560c24c1ab4df353d
Nils Riedemann <nils@bleepbloop.studio>
2023-06-20 20:13:10 +0200

    feat(keychron/v8/ansi:nocksock): updated keymap

--- keyboards/keychron/v8/ansi_encoder/keymaps/nocksock/keymap.c
+++ keyboards/keychron/v8/ansi_encoder/keymaps/nocksock/keymap.c
@@ -5,18 +5,20 @@
 #include "oneshot.h"
 #include "swapper.h"
 
+#define ENCODER_DIRECTION_FLIP
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // NOTE: there are two KC_B keys, so when copying from another layout, make
     // sure to accomodate that.
 
     [BASE] = LAYOUT_ansi_69(
-        KC_ESC  , KC_1    , KC_2    , KC_3   , KC_4   , KC_5   , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , KC_BSPC , KC_MUTE ,
+        KC_ESC  , KC_1    , KC_2    , KC_3   , KC_4   , KC_5   , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , KC_BSPC , KC_MUTE   ,
         KC_TAB  , KC_Q    , KC_W    , KC_E   , KC_R   , KC_T   , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS , TG(COLOR) ,
         KC_LCTL , KC_A    , KC_S    , KC_D   , KC_F   , KC_G   , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT , KC_RTNC , KC_HOME ,
-        KC_LSFT , KC_LTZ    , KC_X    , KC_C   , KC_V   , KC_B   , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , KC_UP   ,
+        KC_LSFT , KC_LTZ  , KC_X    , KC_C   , KC_V   , KC_B   , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , KC_UP   ,
         KC_MEH  , KC_LOPT , KC_LCMD , KC_SPC , LA_NAV , LA_SYM , KC_SESC , KC_RCMD , KC_LEFT , KC_DOWN , KC_RGHT
-    )           ,
+    ) ,
 
     [SYM] = LAYOUT_ansi_69(
         _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
@@ -32,22 +34,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______ , OS_CMD  , OS_ALT  , OS_CTRL , OS_SHFT , KC_VOLD , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_BSPC , _______ , _______ , _______ ,
         _______ , SPACE_L , SPACE_R , BACK    , FWD     , KC_MPLY , _______ , CW_TOGG , KC_PGDN , KC_PGUP , SW_LANG , KC_ENT  , _______ , _______ ,
         _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
-    ),
+    ) ,
 
     [NUM] = LAYOUT_ansi_69(
         _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
         _______ , KC_7    , KC_5    , KC_3    , KC_1    , KC_9    , KC_8    , KC_0    , KC_2    , KC_4    , KC_6    , _______ , _______ , _______ , _______ ,
-        _______ , OS_SHFT , OS_CTRL , OS_ALT  , OS_CMD  , KC_F11  , KC_F12  , OS_CMD  , OS_ALT  , OS_CTRL , OS_SHFT , _______ , _______ , _______ ,
+        _______ , OS_CMD  , OS_ALT  , OS_CTRL , OS_SHFT ,  KC_F11  , KC_F12  , OS_SHFT , OS_CTRL , OS_ALT  , OS_CMD , _______ , _______ , _______ ,
         _______ , KC_F7   , KC_F5   , KC_F3   , KC_F1   , KC_F9   , _______ , KC_F8   , KC_F10  , KC_F2   , KC_F4   , KC_F6   , _______ , _______ ,
         _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
     ),
 
     [NUMPAD] = LAYOUT_ansi_69(
-        _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-        _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX , KC_7    , KC_8    , KC_9    , KC_0    , _______ , _______ , _______ , _______ ,
-        _______ , XXXXXXX , OS_ALT  , OS_CTRL , OS_SHFT , OSM_HYPR , XXXXXXX , KC_4    , KC_5    , KC_6    , OS_CMD  , _______ , _______ , _______ ,
-        _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  , _______ , XXXXXXX , KC_1    , KC_2    , KC_3    , XXXXXXX , _______ , _______ ,
-        _______ , _______ , _______ , _______ , OS_CMD  , _______  , _______ , _______ , _______ , _______ , _______
+        _______ , _______ , _______ , _______ , _______ , _______    , _______    , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+        _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , HYPR(KC_J) , HYPR(KC_O) , KC_7    , KC_8    , KC_9    , KC_0    , _______ , _______ , _______ , _______ ,
+        _______ , XXXXXXX , OS_ALT  , OS_CTRL , OS_SHFT , OSM_HYPR   , XXXXXXX    , KC_4    , KC_5    , KC_6    , OS_CMD  , _______ , _______ , _______ ,
+        _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    , _______    , KC_0    , KC_1    , KC_2    , KC_3    , XXXXXXX , _______ , _______ ,
+        _______ , _______ , _______ , OS_CMD  , _______ ,  _______    , _______    , _______ , _______ , _______ , _______
     ),
 
     [COLOR] = LAYOUT_ansi_69(
